1cde41521b2b36b86a7d7312cf4b0afae5ab90ba
anselmobd <anselmo.blanco.dominguez@gmail.com>
2024-12-24 12:43:20 -0300

    adicionando imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Estamos estudando para ir ao infinito e além!</h1>
+    return <div>
+        <h1>Estamos estudando para ir ao infinito e além!</h1>
+        <img src="/images/rex_study.jpg" alt="Rex estudando" />
+    </div>
 }
 
 export default Home;
