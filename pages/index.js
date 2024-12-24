ecf0a349a2a2a9e0877e46f3040108f0a5652aea
anselmobd <anselmo.blanco.dominguez@gmail.com>
2024-12-24 11:26:41 -0300

    label

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Estamos estudando para chegar ao infinito e além!</h1>
+    return <h1>Estamos estudando para ir ao infinito e além!</h1>
 }
 
 export default Home;
