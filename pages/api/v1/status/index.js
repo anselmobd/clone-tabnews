5ff9c554b96fd56c697f24ca029af178a1d627e0
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-12 20:19:48 -0300

    retornando em `database`, `version`, `max_connections` e `open_connections` e testando

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,29 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  // const result = await database.query("SELECT 1+1 AS sum;");
   const updatedAt = new Date().toISOString();
+
+  const resultServerVersion = await database.query("SHOW server_version;");
+  console.log(resultServerVersion.rows);
+  const serverVersion = resultServerVersion.rows[0].server_version;
+
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  console.log(resultMaxConnections.rows);
+  const maxConnections = resultMaxConnections.rows[0].max_connections;
+
+  const resultOpenConnections = await database.query(
+    "SELECT count(*) open_connections FROM pg_stat_activity;",
+  );
+  console.log(resultOpenConnections.rows);
+  const openConnections = resultOpenConnections.rows[0].open_connections;
+
   response.status(200).json({
     update_at: updatedAt,
-    database: {},
+    database: {
+      version: serverVersion,
+      max_connections: parseInt(maxConnections),
+      open_connections: parseInt(openConnections),
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,8 +7,15 @@ test("GET to /api/v1/status should return 200", async () => {
 
   expect(responseBody.update_at).toBeDefined();
   expect(responseBody.database).toBeDefined();
+  expect(responseBody.database.version).toBeDefined();
+  expect(responseBody.database.max_connections).toBeDefined();
+  expect(responseBody.database.open_connections).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
   console.log("parsedUpdatedAt", parsedUpdatedAt);
   expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.database.version).toMatch(/^[0-9\.]+$/);
+  expect(responseBody.database.max_connections).toBeGreaterThan(0);
+  expect(responseBody.database.open_connections).toBeGreaterThan(0);
 });
