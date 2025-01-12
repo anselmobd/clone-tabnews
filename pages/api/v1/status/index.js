b0bd2bd2e36630d608ccaa4de7e698c0a69f8d84
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-11 21:07:01 -0300

    add files `.env` e `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 AS sum;");
+  console.log(result.rows);
+
   // response.setHeader("Content-Type", "text/plain; charset=utf-8");
   // response.status(200).send("Status: Curso.dev é acima da média");
-
   response.status(200).json({ Status: "OK" });
 }
 
