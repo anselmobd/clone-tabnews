cad443c6795626800167ab2a3cf6571cffb96708
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-13 23:11:47 -0300

    test database.version toBe 16.6

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -6,17 +6,19 @@ test("GET to /api/v1/status should return 200", async () => {
   console.log("responseBody", responseBody);
 
   expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  console.log("parsedUpdatedAt", parsedUpdatedAt);
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+
   expect(responseBody.dependencies).toBeDefined();
   expect(responseBody.dependencies.database).toBeDefined();
   expect(responseBody.dependencies.database.version).toBeDefined();
   expect(responseBody.dependencies.database.max_connections).toBeDefined();
   expect(responseBody.dependencies.database.open_connections).toBeDefined();
 
-  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
-  console.log("parsedUpdatedAt", parsedUpdatedAt);
-  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
-
   expect(responseBody.dependencies.database.version).toMatch(/^[0-9\.]+$/);
+  expect(responseBody.dependencies.database.version).toBe("16.6");
   expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
   expect(responseBody.dependencies.database.open_connections).toBeGreaterThan(
     0,
