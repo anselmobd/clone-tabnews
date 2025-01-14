03db366190c1d63043870516fc1378c2e639f0e7
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-14 00:22:35 -0300

    utilizando `getFirstRowColumn`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -20,15 +20,15 @@ async function status(request, response) {
     "server_version",
   );
 
-  const resultMaxConnections = await database.query("SHOW max_connections;");
-  console.log(resultMaxConnections.rows);
-  const maxConnections = resultMaxConnections.rows[0].max_connections;
-
-  const resultOpenConnections = await database.query(
-    "SELECT count(*) open_connections FROM pg_stat_activity;",
+  const maxConnections = await getFirstRowColumn(
+    "SHOW max_connections;",
+    "max_connections",
   );
-  console.log(resultOpenConnections.rows);
-  const openConnections = resultOpenConnections.rows[0].open_connections;
+
+  const openConnections = await getFirstRowColumn(
+    "SELECT count(*) FROM pg_stat_activity;",
+    "count",
+  );
 
   response.status(200).json({
     update_at: updatedAt,
