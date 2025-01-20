aaeb1c593ee512cf123f7b23bf1e9c56fb779072
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-20 00:16:37 -0300

    adaptado às novas funções de database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,20 +3,21 @@ import database from "/infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const serverVersion = await database.getFirstRowColumn(
-    "SHOW server_version;",
-    "server_version",
-  );
+  const serverVersion = await database.getFirstRowColumn({
+    sql: "SHOW server_version;",
+    name: "server_version",
+  });
 
-  const maxConnections = await database.getFirstRowColumn(
-    "SHOW max_connections;",
-    "max_connections",
-  );
+  const maxConnections = await database.getFirstRowColumn({
+    sql: "SHOW max_connections;",
+    name: "max_connections",
+  });
 
-  const openConnections = await database.getFirstRowColumn(
-    "SELECT count(*) FROM pg_stat_activity WHERE datname = 'local_db';",
-    "count",
-  );
+  const openConnections = await database.getFirstRowColumn({
+    sql: "SELECT count(*)::INT FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+    name: "count",
+  });
 
   response.status(200).json({
     update_at: updatedAt,
@@ -24,7 +25,7 @@ async function status(request, response) {
       database: {
         version: serverVersion,
         max_connections: parseInt(maxConnections),
-        open_connections: parseInt(openConnections),
+        open_connections: openConnections,
       },
     },
   });
