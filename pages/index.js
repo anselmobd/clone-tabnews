b3085d79d72059a5a7c9091c71b28841122662bd
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-20 18:19:38 -0300

    mensagem citando `Tutorial.dev.br`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 function Home() {
   return (
     <div>
-      <h1>Estamos estudando para ir ao infinito e além!</h1>
+      <h1>Crie e divulge seus tutoriais!</h1>
+      <h2>
+        Tem gente (e dinossauros) esperando seus ensinamentos para irem ao
+        infinito e além!
+      </h2>
+
       <img src="/images/rex_study.jpg" alt="Rex estudando" />
+      <p>
+        <b>Tutorial.dev.br</b> é uma ferramenta para desenvolvimento de
+        tutoriais com a ajuda, via comentários, de interessados no assunto.
+      </p>
+      <h3>A primeira versão será liberada em breve!</h3>
+      <p>Guarde este link e não esqueça de retornar.</p>
+      <h1>
+        <a href="https://tutorial.dev.br">tutorial.dev.br</a>
+      </h1>
     </div>
   );
 }
