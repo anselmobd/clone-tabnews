ba5315efc493ca9eaa104c9ae6eb2671f5abdba6
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-26 17:56:39 -0300

    create and test POST migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,9 +2,18 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
+  let dryRun;
+  if (request.method === "GET") {
+    dryRun = true;
+  } else if (request.method === "POST") {
+    dryRun = false;
+  } else {
+    response.status(405).json({ message: "Method not allowed" });
+    return;
+  }
   const migrations = await migrationRunner({
     databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
+    dryRun: dryRun,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
