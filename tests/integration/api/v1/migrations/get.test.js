2402b4c8b7468c3deb99d40e3e215c8625adfa8b
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-26 21:53:21 -0300

    import database and test `process.env.DATABASE_UR`

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,4 +1,7 @@
+import database from "infra/database.js";
+
 test("GET to /api/v1/migrations should return 200", async () => {
+  console.log(process.env.DATABASE_URL);
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
