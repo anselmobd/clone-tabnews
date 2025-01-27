843c0819570e1d2c5bafd9bca74719d2a297e8b8
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-26 22:12:37 -0300

    create ans call function `cleanDatabase`before get test

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,5 +1,11 @@
 import database from "infra/database.js";
 
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.rawQuery("drop schema public cascade; create schema public;");
+}
+
 test("GET to /api/v1/migrations should return 200", async () => {
   console.log(process.env.DATABASE_URL);
   const response = await fetch("http://localhost:3000/api/v1/migrations");
