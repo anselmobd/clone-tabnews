a5c8c207e21ee8c4954ef8c7454e61604fe3a143
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-26 22:39:24 -0300

    POST test also with `cleanDatabase`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,11 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.rawQuery("drop schema public cascade; create schema public;");
+}
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
@@ -5,7 +13,14 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const responseGet = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(responseGet.status).toBe(200);
+  const responseGetBody = await responseGet.json();
+  expect(responseGetBody.length).toBe(0);
 });
