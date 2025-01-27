a2567f725b834fbdf3b919b2519c1713d386f4ae
anselmobd <anselmo.blanco.dominguez@gmail.com>
2025-01-26 23:06:04 -0300

    refactored

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,22 +2,32 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  let dryRun;
-  if (request.method === "GET") {
-    dryRun = true;
-  } else if (request.method === "POST") {
-    dryRun = false;
-  } else {
-    response.status(405).json({ message: "Method not allowed" });
-    return;
-  }
-  const migrations = await migrationRunner({
+  const defaultMigrationOptions = {
     databaseUrl: process.env.DATABASE_URL,
-    dryRun: dryRun,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations",
-  });
-  response.status(200).json(migrations);
+  };
+
+  if (request.method === "GET") {
+    const migrations = await migrationRunner({
+      ...defaultMigrationOptions, // Spreat / Espalha
+      dryRun: true,
+    });
+    return response.status(200).json(migrations);
+  } else if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      ...defaultMigrationOptions, // Spreat / Espalha
+      dryRun: false,
+    });
+
+    if (migrations.length === 0) {
+      return response.status(200).json(migrations);
+    }
+
+    return response.status(201).json(migrations);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
